ee23e92dd20aebdb2d48b276f679fa9de0558b9a
Fernando Massi <massi.fhm@gmail.com>
2025-02-18 11:38:46 -0300

    Definida a tela cadastroVIEW como a principal do projeto

---
+++ LeiloesTDSat/src/Inicial.java
@@ -0,0 +1,9 @@
+public class Inicial {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new cadastroVIEW().setVisible(true);
+            }
+        });
+    }
+}
